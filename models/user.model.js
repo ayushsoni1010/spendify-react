6297da2a16945b3bc2ddbba900608af588e8cba5
Ayush Soni <mr.soni1002001@gmail.com>
2022-09-16 23:24:40 +0530

    update: user data model

--- models/user.model.js
+++ models/user.model.js
@@ -2,15 +2,31 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
+    firstName: {
+      type: String,
+      required: [true, "Please add the first name"],
+      unique: true,
+      trim: true,
+      minlength: 1,
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please add the last name"],
+      unique: true,
+      trim: true,
+      minlength: 1,
+    },
     email: {
       type: String,
       required: [true, "Please add an email"],
       unique: true,
       trim: true,
+      minlength: 3,
     },
     password: {
       type: String,
       required: [true, "Please add a password"],
+      minlength: 3,
     },
   },
   {
